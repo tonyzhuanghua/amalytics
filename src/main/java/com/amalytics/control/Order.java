a8873da22139c33f3a2e71fe8e7be63c3b7de934
zhuanghua <tonyzhuanghua@gmail.com>
2018-01-06 17:41:45 +0000

    add comments

--- src/main/java/com/amalytics/control/Order.java
+++ src/main/java/com/amalytics/control/Order.java
@@ -14,8 +14,18 @@ import java.util.stream.Collectors;
  */
 public class Order {
 
+    // 这个是order的一个固有变量,记录了订单里每个具体的商品,某种商品可能不止一个
+    // 这些商品集合是一个无需顺序的单个item集合,里面包含重复的item, 比如 apple, orange, apple, papaya, garlic, orange....
     private List<Item> items = new ArrayList<>();
+
+    // itemGroup是店家预先设置的,里面包含了两个重要的Map类: 商品列表Map(不会重复),以及对应的折扣信息Map
+    //为了将【商品列表】和【折扣列表】对应起来,这两个变量都是map,而且都是以商品名称作为key
     private ItemGroup itemGroup;
+
+
+
+    // 这个构造函数是以 itemGroup 为参数构造的,意味着在实例化这个Order类之前,itemGroup必须已经准备好了
+    // 也就是说,折扣信息在订单前就存在
 
     public Order(ItemGroup itemGroup) {
         this.itemGroup = itemGroup;
@@ -26,23 +36,36 @@ public class Order {
     }
 
 
+    //这个add方法是将购物篮里面的物品逐一加到订单了去,那么这里面物品就允许重复
     public void add(List<String> itemNames) {
 
         itemNames.forEach((itemName) -> {
 
-            Item inventoryItem = itemGroup.getListingItems().get(itemName);
+            Item item = itemGroup.getListingItems().get(itemName);
 
-            items.add(new Item(inventoryItem.getName(), inventoryItem.getPrice()));
+            items.add(new Item(item.getName(), item.getPrice()));
 
         });
     }
 
+
+
+    //对这个订单实例进行实际价格的运算
     public double calFinalPrice() {
         double finalPrice = 0;
-        Map<String, List<Item>> groupItems = items.stream().collect(Collectors.groupingByConcurrent(Item::getName));
 
+        //这一步是将购物篮里的商品按照商品名做主键,商品集合作为值, 生成一个map, 用来计算最后价格
+        // map的键是唯一的商品名, 值是订单里这个商品集合, 该商品集合讲在下面,使用对于的折扣策略来计算总价
+        Map<String, List<Item>> groupItems = items.stream()
+                .collect(Collectors.groupingBy(Item::getName));
+
+        //遍历上面的map, 对每一类商品算一个总价
         for (Map.Entry<String, List<Item>> listItemEntry : groupItems.entrySet()) {
-            finalPrice += itemGroup.getDiscounts().get(listItemEntry.getKey()).calculatePrice(listItemEntry.getValue());
+            finalPrice += itemGroup.getDiscounts() // 返回总的折扣策略集合
+                    .get(listItemEntry.getKey()) // 获得该种商品的具体折扣策略,返回的是一个DiscountStrategy实例
+                    .calculatePrice(listItemEntry.getValue()); //由折扣策略类计算出N个同类商品最后折扣价格
+                                                               // 折扣策略类的主要功能就是算出N个同类商品,打折后的价格
+                                                               // 所以订单里某个商品的集合的总价是交给策略类来算的
         }
 
         return finalPrice;
@@ -51,10 +74,11 @@ public class Order {
 
     public double calOrigianlPrice() {
         double originalPrice = 0;
-        Map<String, List<Item>> groupItems = items.stream().collect(Collectors.groupingByConcurrent(Item::getName));
+        Map<String, List<Item>> groupItems = items.stream().collect(Collectors.groupingBy(Item::getName));
 
         for (Map.Entry<String, List<Item>> listItemEntry : groupItems.entrySet()) {
-            originalPrice += itemGroup.defaultDiscountSetting().calculatePrice(listItemEntry.getValue());
+            originalPrice += itemGroup.defaultDiscountSetting() //返回无折扣策略
+                    .calculatePrice(listItemEntry.getValue());
         }
 
         return originalPrice;
--- src/main/java/com/amalytics/entity/ItemGroup.java
+++ src/main/java/com/amalytics/entity/ItemGroup.java
@@ -10,10 +10,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
+/*
+* 这个类是商家设置好的,商品打折集合对照表。买家下订单后,商家要对照这个类来计算折扣
+* 这个类其实是由商品, 打折名称两个Map, 生成一个 <商品名, 打折策略> 的map
+* 生成这个打折策略map过程在构造函数中,使得这个类实例化后,打折策略map已经生成
+* 打折策略抽象类DiscountStrategy只有一个计算价格的方法
+* 所以逻辑关系可以以下表述
+* 商品Item类 + 商品打折信息 -> 商品打折策略 + 订单 -> 计算出价格
+* */
 public class ItemGroup {
 
+    // 商家准备好的商品列表, key是商品名称不可重复, item是该商品名称对应的商品实例
     private Map<String, Item> listingItems = new HashMap<String, Item>();
+    // 这里的 DiscountType 是enum类里定义的折扣名称
     private Map<String, DiscountType> discountTypes = new HashMap<>();
+    // 下面这个变量是由构造函数生成的,
     private Map<String, DiscountStrategy> discounts = new HashMap<>();
 
     private NoDiscount noDiscount;
@@ -35,6 +46,7 @@ public class ItemGroup {
         return new NoDiscount();
     }
 
+    // 该构造函数生成打折策略map <商品名, 打折策略>
     public ItemGroup(List<Item> items, List<DiscountType> discountTypeList) {
 
         noDiscount = new NoDiscount();
